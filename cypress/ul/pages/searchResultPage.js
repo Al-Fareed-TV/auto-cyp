5ae169f6e0d34d640b7c5b781f3321a807d95190
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2024-05-22 21:31:05 +0530

    validates name of the listed items in search result page

--- cypress/ul/pages/searchResultPage.js
+++ cypress/ul/pages/searchResultPage.js
@@ -1,7 +1,16 @@
 class searchResultPage{
-    
     selectFirstItem() {
         cy.get("#product-grid > ul > li:nth-child(1)").click();
+    }
+    checkNameOfItemListed(text) {
+        cy.get(
+          "#product-grid > ul > li:nth-child(1) > div > div.card-information > div > h3 > a"
+        )
+          .invoke("text")
+          .then((text) => {
+            cy.log(text);
+            expect(text).to.exist;
+          });
     }
 }
 module.exports = searchResultPage;
